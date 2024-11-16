ffb3510b4e8bb1dc7a4881e8565d376499f36bb3
IKesh Pack <ikesh@devangels.london>
2024-11-16 18:31:24 -0500

    first commit

---
+++ lib/components/header.dart
@@ -0,0 +1,54 @@
+import 'package:jaspr/jaspr.dart';
+import 'package:jaspr_router/jaspr_router.dart';
+
+import '../constants/theme.dart';
+
+class Header extends StatelessComponent {
+  const Header({super.key});
+
+  @override
+  Iterable<Component> build(BuildContext context) sync* {
+    var activePath = context.binding.currentUri.path;
+
+    yield header([
+      nav([
+        div(classes: 'logo', [
+          Link(to: '/', child: img(src: 'icons/logo.svg', alt: 'logo', width: 35, height: 39)),
+        ]),
+        div(classes: 'horizontal-spacer', []),
+        for (var route in [
+          (label: 'Home', path: '/'),
+          (label: 'Experience', path: '/experience'),
+          (label: 'Projects', path: '/projects'),
+          (label: 'About', path: '/about'),
+        ])
+          div(classes: 'nav-item ${activePath == route.path ? 'active' : ''}', [
+            Link(to: route.path, child: text(route.label)),
+          ]),
+      ]),
+    ]);
+  }
+
+  @css
+  static final styles = [
+    css('header', [
+      css('&').background(color: eerieBlack),
+      css('nav', [
+        css('&')
+            .box(
+              maxWidth: 1160.px,
+              margin: EdgeInsets.symmetric(horizontal: Unit.auto, vertical: 37.px),
+              display: Display.flex,
+            )
+            .flexbox(alignItems: AlignItems.center),
+        css('div', [
+          css('&').box(display: Display.inline),
+          css('&.horizontal-spacer').flexItem(flex: const Flex(grow: 1)),
+          css('&.nav-item') //
+              .box(margin: EdgeInsets.only(left: 40.px)),
+          css('a').text(fontSize: 18.px, color: white, decoration: TextDecoration.none),
+        ]),
+      ]),
+    ]),
+  ];
+}
---
+++ lib/constants/theme.dart
@@ -0,0 +1,11 @@
+import 'package:jaspr/jaspr.dart';
+
+// As your css styles are defined using just Dart, you can simply
+// use global variables or methods for common things like colors.
+const primaryColor = Color.hex('#01589B');
+
+/// Eerie Black.
+const eerieBlack = Color.hex('#1A0B2E');
+
+/// White.
+const white = Color.hex('#FFFFFF');
