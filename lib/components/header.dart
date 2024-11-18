57a67cd944c60aae9842e7ae6095a8e96051de10
IKesh Pack <ikesh@devangels.london>
2024-11-17 20:14:57 -0500

    fix: use flexbox for header

--- lib/components/header.dart
+++ lib/components/header.dart
@@ -1,10 +1,18 @@
 import 'package:jaspr/jaspr.dart';
 import 'package:jaspr_router/jaspr_router.dart';
+import 'package:portfolio_jaspr/constants/layout.dart';
 
 import '../constants/theme.dart';
 
 class Header extends StatelessComponent {
   const Header({super.key});
+
+  static const _routes = [
+    (label: 'Home', path: '/'),
+    (label: 'Experience', path: '/experience'),
+    (label: 'Projects', path: '/projects'),
+    (label: 'About', path: '/about'),
+  ];
 
   @override
   Iterable<Component> build(BuildContext context) sync* {
@@ -13,18 +21,17 @@ class Header extends StatelessComponent {
     yield header([
       nav([
         div(classes: 'logo', [
-          Link(to: '/', child: img(src: 'icons/logo.svg', alt: 'logo', width: 35, height: 39)),
+          Link(
+            to: '/',
+            child: img(src: 'icons/logo.svg', alt: 'logo', width: 35, height: 39),
+          ),
         ]),
-        div(classes: 'horizontal-spacer', []),
-        for (var route in [
-          (label: 'Home', path: '/'),
-          (label: 'Experience', path: '/experience'),
-          (label: 'Projects', path: '/projects'),
-          (label: 'About', path: '/about'),
-        ])
-          div(classes: 'nav-item ${activePath == route.path ? 'active' : ''}', [
-            Link(to: route.path, child: text(route.label)),
-          ]),
+        div(classes: 'nav-items', [
+          for (var route in _routes)
+            div(classes: 'nav-item ${activePath == route.path ? 'active' : ''}', [
+              Link(to: route.path, child: text(route.label)),
+            ]),
+        ]),
       ]),
     ]);
   }
@@ -32,22 +39,40 @@ class Header extends StatelessComponent {
   @css
   static final styles = <StyleRule>[
     css('header', [
-      css('&').background(color: eerieBlack),
+      css('&') //
+          .background(color: eerieBlack)
+          .box(display: Display.flex)
+          .flexbox(justifyContent: JustifyContent.center),
       css('nav', [
         css('&')
             .box(
-              maxWidth: 1160.px,
-              margin: EdgeInsets.symmetric(horizontal: Unit.auto, vertical: 37.px),
+              maxWidth: maxWidth,
+              margin: EdgeInsets.symmetric(
+                horizontal: 16.px,
+                vertical: 37.px,
+              ),
               display: Display.flex,
             )
-            .flexbox(alignItems: AlignItems.center),
-        css('div', [
-          css('&').box(display: Display.inline),
-          css('&.horizontal-spacer').flexItem(flex: const Flex(grow: 1)),
-          css('&.nav-item', [
-            css('&').box(margin: EdgeInsets.symmetric(horizontal: 40.px)),
+            .flexbox(
+              alignItems: AlignItems.center,
+              justifyContent: JustifyContent.spaceBetween,
+            )
+            .flexItem(flex: flexGrow1),
+        css('.nav-items', [
+          css('&') //
+              .box(display: Display.flex)
+              .flexbox(
+                alignItems: AlignItems.center,
+                justifyContent: JustifyContent.end,
+              ),
+          css('.nav-item', [
+            css('&').box(margin: EdgeInsets.only(left: 20.px)),
             css('&.active a').text(color: grape),
-            css('a').text(fontSize: 18.px, color: white, decoration: TextDecoration.none),
+            css('a').text(
+              fontSize: 18.px,
+              color: white,
+              decoration: TextDecoration.none,
+            ),
           ]),
         ]),
       ]),
---
+++ lib/constants/layout.dart
@@ -0,0 +1,7 @@
+import 'package:jaspr/jaspr.dart';
+
+/// max-width of the page.
+final maxWidth = 1160.px;
+
+/// flex grow of 1.
+const flexGrow1 = Flex(grow: 1);
